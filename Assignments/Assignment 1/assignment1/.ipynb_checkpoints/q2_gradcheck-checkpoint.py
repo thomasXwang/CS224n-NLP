2e337f6c0d92e014dfb3e22149eacfacad3821e5
Thomas Wang <twang@assystemtechnologies.com>
2019-10-28 18:05:10 +0100

    modified a bit q2_gradhcheck, still have to correct

--- Assignments/Assignment 1/assignment1/.ipynb_checkpoints/q2_gradcheck-checkpoint.py
+++ Assignments/Assignment 1/assignment1/.ipynb_checkpoints/q2_gradcheck-checkpoint.py
@@ -37,21 +37,25 @@ def gradcheck_naive(f, x):
         # to test cost functions with built in randomness later.
 
         ### YOUR CODE HERE:
-        raise NotImplementedError
+        random.setstate(rndstate)
+        print(x[ix])
+        print(f(x[ix]+h))
+        numgrad = (f(x[ix]+h) - f(x[ix]-h) )/(2*h)
+        #raise NotImplementedError
         ### END YOUR CODE
 
         # Compare gradients
         reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
         if reldiff > 1e-5:
-            print "Gradient check failed."
-            print "First gradient error found at index %s" % str(ix)
-            print "Your gradient: %f \t Numerical gradient: %f" % (
-                grad[ix], numgrad)
+            print ("Gradient check failed.")
+            print ("First gradient error found at index %s" % str(ix))
+            print ("Your gradient: %f \t Numerical gradient: %f" % (
+                grad[ix], numgrad))
             return
 
         it.iternext() # Step to next dimension
 
-    print "Gradient check passed!"
+    print ("Gradient check passed!")
 
 
 def sanity_check():
@@ -60,11 +64,11 @@ def sanity_check():
     """
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
-    print "Running sanity checks..."
+    print ("Running sanity checks...")
     gradcheck_naive(quad, np.array(123.456))      # scalar test
     gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
     gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
-    print ""
+    print ("")
 
 
 def your_sanity_checks():
@@ -74,9 +78,9 @@ def your_sanity_checks():
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print "Running your sanity checks..."
+    print ("Running your sanity checks...")
     ### YOUR CODE HERE
-    raise NotImplementedError
+    #raise NotImplementedError
     ### END YOUR CODE
 
 
