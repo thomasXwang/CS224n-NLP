94ddf853e04d8a535bc4235dfa0ea594b1deaf82
Thomas Wang <250395@live.fr>
2019-12-05 14:54:46 +0100

    Assignment 2: Implemented 1.(c)

--- Assignments/Assignment 2/assignment2/q1_classifier.py
+++ Assignments/Assignment 2/assignment2/q1_classifier.py
@@ -46,6 +46,14 @@ class SoftmaxModel(Model):
             self.labels_placeholder
         """
         ### YOUR CODE HERE
+
+        n_features = self.config.n_features
+        n_classes = self.config.n_classes
+        batch_size = self.config.batch_size
+
+        self.input_placeholder = tf.placeholder(tf.float32, shape=(batch_size, n_features))
+        self.labels_placeholder = tf.placeholder(tf.int32, shape=(batch_size, n_classes))
+
         ### END YOUR CODE
 
     def create_feed_dict(self, inputs_batch, labels_batch=None):
@@ -69,6 +77,13 @@ class SoftmaxModel(Model):
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
         ### YOUR CODE HERE
+
+        feed_dict = {}
+
+        feed_dict[self.input_placeholder] = inputs_batch
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch
+
         ### END YOUR CODE
         return feed_dict
 
